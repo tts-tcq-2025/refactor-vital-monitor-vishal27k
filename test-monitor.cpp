fcca485bb5a736d6994f9af995197204da9b1110
vishal27k <52737349+vishal27k@users.noreply.github.com>
2025-08-18 09:05:56 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,6 +2,7 @@
 #include "./monitor.h"
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+  ASSERT_FALSE(handleVitals(99, 102, 70));
+  ASSERT_TRUE(handleVitals(98.1, 70, 98));
 }
+
